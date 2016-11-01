2b9b1696a0102b07a97e860136559487400bd178
heroldus <sebastian.herold@immobilienscout24.de>
2016-11-01 22:10:31 +0100

    DATA-9879 keep a version of every commit

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,14 @@ if [ -e $TMP_ZIP ]; then rm $TMP_ZIP; fi
 # create zip
 zip -v -r $TMP_ZIP *.py requests
 
+# get current git commit hash
+COMMIT_HASH=$(git rev-parse HEAD)
+
 # upload zip file
 aws s3 cp $TMP_ZIP $S3_URL/
+aws s3 cp $TMP_ZIP $S3_URL/$COMMIT_HASH/
 
 # upload template file
 aws s3 cp ../ssl-certificate.template.yaml $S3_URL/
+aws s3 cp ../ssl-certificate.template.yaml $S3_URL/$COMMIT_HASH/
 
