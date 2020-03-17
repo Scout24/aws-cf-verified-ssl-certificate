9490981b126330c95c146871b0ccd7d7bd5a66c2
cdietze <christian.dietze@scout24.com>
2020-03-17 10:53:43 +0100

    Remove the need for CFN-Sphere

--- deploy.sh
+++ deploy.sh
@@ -5,27 +5,35 @@ set -x -e
 python test/python/process_cert_manager_test.py
 
 
-
-S3_URL=${1:-s3://is24-infrastructure-public/cloudformation/verified-ssl-certificate}
+S3_BUCKET=${1:-is24-infrastructure-public}
+CREATE_INFRASTRUCTURE=${2:-true}
+S3_URL=s3://${S3_BUCKET}/cloudformation/verified-ssl-certificate
 TMP_ZIP=$TMPDIR/lambda_functions.zip
 
 cd lambda/
 
 # if tmp file exists, delete it
-if [ -e $TMP_ZIP ]; then rm $TMP_ZIP; fi
+if [ -e "$TMP_ZIP" ]; then rm "$TMP_ZIP"; fi
 
 # create zip
 pip install -t . requests
-zip -v -r $TMP_ZIP .
+zip -v -r "$TMP_ZIP" .
 
 # get current git commit hash
 COMMIT_HASH=$(git rev-parse HEAD)
 
 # upload zip file
 aws s3 cp $TMP_ZIP $S3_URL/
-aws s3 cp $TMP_ZIP $S3_URL/$COMMIT_HASH/
+aws s3 cp $TMP_ZIP $S3_URL/"$COMMIT_HASH"/
 
 # upload template file
-aws s3 cp ../ssl-certificate.template.yaml $S3_URL/
-aws s3 cp ../ssl-certificate.template.yaml $S3_URL/$COMMIT_HASH/
+aws s3 cp ../ssl-certificate.template.yaml "$S3_URL"/
+aws s3 cp ../ssl-certificate.template.yaml "$S3_URL"/"$COMMIT_HASH"/
 
+
+if [ "${CREATE_INFRASTRUCTURE}" == "true" ]
+then
+  aws cloudformation deploy --template global-hosting.template.yaml --parameter-overrides S3BucketName="${S3_BUCKET}" \
+    --tags segment=infrastructure usecase=cf-verified-ssl-certificate \
+    --stack-name global-cf-verified-ssl-certificate-hosting
+fi
